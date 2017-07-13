ccb973f4fcdcbfcf35a26b594b3de05e657612c9
Guillermo Guirao Aguilar <contact@guillermoguiraoaguilar.com>
2017-07-13 23:30:29 +0200

    Changed default port

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -7,7 +7,7 @@ import _ from 'lodash';
 var all = {
   env: process.env.NODE_ENV,
   ip: process.env.IP || '0.0.0.0',
-  port: process.env.PORT || 80,
+  port: process.env.PORT || 8080,
   root: path.normalize(`${__dirname}/../../..`),
   browserSyncPort: process.env.BROWSER_SYNC_PORT || 3000,
   api: process.env.API_URL,
