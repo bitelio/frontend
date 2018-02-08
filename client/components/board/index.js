561915ff336ff5ef015de0293cc3ca84a643feff
Guillermo Guirao Aguilar <contact@guillermoguiraoaguilar.com>
2018-02-08 19:09:16 +0100

    Removed board component

--- client/components/board/index.js
+++ client/components/board/index.js
@@ -2,7 +2,6 @@
 
 import angular from 'angular';
 import BoardService from './board.service';
-import BoardComponent from './board.component';
 
 export default angular.module('bitelio.components.board', [])
   .service('Board', BoardService)
