88d606fc493b918cf397a7a8a128dfdea1394bac
Guillermo Guirao Aguilar <contact@guillermoguiraoaguilar.com>
2018-05-20 20:43:16 +0200

    Removed node backend logic

--- server/routes.js
+++ server/routes.js
@@ -2,18 +2,12 @@
 
 import path from 'path';
 import httpProxy from 'http-proxy';
-import bodyParser from 'body-parser';
 import env from 'config/environment';
-import {isAuthenticated} from './auth/auth.service';
 
 module.exports = function(app) {
   var proxy = httpProxy.createProxyServer({proxyTimeout: 3000});
 
-  app.use('/api/user', isAuthenticated(), bodyParser.json(), require('./user'));
-
-  app.use('/api/auth', bodyParser.json(), require('./auth'));
-
-  app.all('/api/*', isAuthenticated(), (req, res, next) => {
+  app.all('/api/*', (req, res, next) => {
     proxy.web(req, res, {target: env.api}, err => next(err));
   });
 
