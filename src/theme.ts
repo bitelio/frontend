1c0fed7ea2647fdf555bcdca8d4986bb73fec03a
Guillermo Guirao Aguilar <contact@guillermoguiraoaguilar.com>
2020-04-05 20:13:55 +0200

    Added basic theme

---
+++ src/theme.ts
@@ -0,0 +1,18 @@
+import { red, blue } from "@material-ui/core/colors";
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: blue.A700
+    },
+    error: {
+      main: red.A400
+    },
+    background: {
+      default: "#000"
+    }
+  }
+});
+
+export default theme;
